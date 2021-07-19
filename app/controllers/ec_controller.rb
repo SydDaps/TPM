5d8dade5f0c8680e8b83a6ca77ab0ff576eb1f63
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-07-19 23:37:32 +0000

    Added Maria db

--- app/controllers/ec_controller.rb
+++ app/controllers/ec_controller.rb
@@ -1,5 +1,6 @@
 class EcController < ApplicationController
     def index
-        @ec_users = ElectoralCommission.all
+        #@ec_users = ElectoralCommission.all
+        @ec_users = EcUser.all
     end
 end
